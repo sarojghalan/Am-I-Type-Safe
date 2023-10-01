2dc0fb93336acb8d7bad1e45afadfcfb00e66092
Hypekage <jmestmnag@gmail.com>
2023-10-01 20:12:28 +0545

    starting from ` basic `.

---
+++ ts-basic.ts
@@ -0,0 +1,30 @@
+// let user: string = "Awakened Mutant";
+// let num: number = 2;
+
+// interface ObjSetter {
+//   id: number;
+//   name: string;
+//   regexValue: RegExp;
+// }
+
+// const obj: ObjSetter = {
+//   id: 1,
+//   name: "Awakened Mutant",
+//   regexValue: /foo/,
+// };
+
+// type Valid = "true" | "false";
+
+// const validChecker: Record<Valid, string> = {
+//   true: "true",
+//   false: "false",
+// };
+
+const ids = {
+  10: "ten",
+  20: "twenty",
+  30: "thirty",
+};
+// ids[40] = "forty";
+console.log(ids);
+
