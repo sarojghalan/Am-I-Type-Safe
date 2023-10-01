6d0c189ad14c3870e9ff4b31a1eae68be653bc5d
Hypekage <jmestmnag@gmail.com>
2023-10-01 21:28:03 +0545

    starting form basic `interface` | `record`

--- ts-basic.ts
+++ ts-basic.ts
@@ -1,30 +1,30 @@
-// let user: string = "Awakened Mutant";
-// let num: number = 2;
+let user: string = "Awakened Mutant";
+let num: number = 2;
 
-// interface ObjSetter {
-//   id: number;
-//   name: string;
-//   regexValue: RegExp;
-// }
+interface ObjSetter {
+  id: number;
+  name: string;
+  regexValue: RegExp;
+}
 
-// const obj: ObjSetter = {
-//   id: 1,
-//   name: "Awakened Mutant",
-//   regexValue: /foo/,
-// };
+const obj: ObjSetter = {
+  id: 1,
+  name: "Awakened Mutant",
+  regexValue: /foo/,
+};
 
-// type Valid = "true" | "false";
+type Valid = "true" | "false";
 
-// const validChecker: Record<Valid, string> = {
-//   true: "true",
-//   false: "false",
-// };
+const validChecker: Record<Valid, string> = {
+  true: "true",
+  false: "false",
+};
 
 const ids = {
   10: "ten",
   20: "twenty",
   30: "thirty",
 };
-// ids[40] = "forty";
+
 console.log(ids);
 
